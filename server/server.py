b42e90db4c3d2d702c99a4bf02c0da8d8023d0bb
plutozonexyz <admin@plutozone.xyz>
2020-08-05 11:05:51 -0400

    Update stuff, add functions for stuff

--- server/server.py
+++ server/server.py
@@ -3,7 +3,6 @@ import pgpy
 import threading
 import select
 
-sel = select.DefaultSelector()
 
 CCOMMPORT = 293
 HEADSIZE = 20
@@ -11,6 +10,11 @@ HEADSIZE = 20
 servsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servsock.bind(('', CCOMMPORT))
 servsock.listen()
+print(f'listening on *:{CCOMMPORT}'))
+serversock.setblocking(False)
+
+sel.register(serversock, selectors.EVENT_READ, accept)
+
 
 # while True:
 #     msgf = f'{len(msg):<{HEADSIZE}}'+msg
@@ -19,4 +23,29 @@ servsock.listen()
 #     clientsocket.send(bytes(msgf, "UTF-8"))
 #     print(clientsocket.recv(1024).decode("UTF-8"))
 
+# clientsocket, address = servsock.accept()
 
+sockets_list = ''
+
+def recv_msg(client_socket):
+    try:
+        msg_head = client_socket.recv(HEADSIZE)
+        if not len(msg_head):
+            return False
+        MSGSIZE = int(msg_head.decode("UTF-8"))
+        return {"header": msg_head, "data": client_socket.recv(MSGSIZE)}
+    except:
+        return False
+
+while True:
+    read_sockets, _, exception_sock = select.select(sockets_list, [], sockets_list)
+    
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = serversock.accept()
+            user = recv_msg(client_socket)
+            if user is False:
+                continue
+            sockets_list.append(client_socket)
+            clients[client_socket] = user
+            print(f"connection {client_address[0]}:{client_address[1]} established")
