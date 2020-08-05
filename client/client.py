2d55e76675a73dc4158486333f00e8b175877c00
plutozonexyz <admin@plutozone.xyz>
2020-08-05 16:45:19 -0400

    fix keypub key err

--- client/client.py
+++ client/client.py
@@ -123,6 +123,7 @@ def keygen():
 
 def keypub(passphrase):
     print("Publishing Keys...")
+    key,_ = pgpy.PGPKey.from_file('./keys/mine/private.key')
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, 293))
     msg = "KEYPUB "+USRNM, 
