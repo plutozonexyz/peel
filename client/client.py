5a577a9d386b00cf217aa6774db0a928f4fef323
plutozonexyz <admin@plutozone.xyz>
2020-08-05 14:42:57 -0400

    added most of compose

--- client/client.py
+++ client/client.py
@@ -3,10 +3,11 @@ import pgpy
 import sys
 import os
 import tarfile
+import hashlib
 from conf import *
 
 HEADSIZE = 20
-FILENMSIZE = 100
+FILENMSIZE = 150
 
 def decrypt_tar(enfile, passphrase):
     enc_tar = pgpy.PGPMessage.from_file('./rx/enc/'+enfile+'.tar.pgp')
@@ -32,22 +33,36 @@ def fetch_msgs(passphrase):
     sock.connect((HOST, 293))
     key,_ = pgpy.PGPKey.from_file('./keys/mine/private.key')
     key.unlock(passphrase)
-    msg = "FETCH "+USRNM, key.sign("MSGS"))
+    msg = "FETCH "+USRNM, key.sign("MSGS")
     msghead = len(msg)
     sock.send(f"{msghead:<{HEADSIZE}}{msg}")
     recvhead = int(sock.recv(HEADSIZE).decode("UTF-8"))
     recvmsg = sock.recv(recvhead).decode("UTF-8")
-    if 'ERROR' in recvmsg[10:]:
-        if recvmsg[-10:] == '1':
+    if 'ERROR' in recvmsg[HEADER:]:
+        if recvmsg[-HEADER:] == '1':
             print(f"No messages avalible for {USRNM}.")
         else:
-            print(f"Got error code {recvmsg[-10:]}.")
-    elif 'RECVMSG' in recvmsg[10:]:
-        print(f"You have {recvmsg[-10:]} new message(s)!")
-        for i in range(int(recvmsg[-10:])):
+            print(f"Got error code {recvmsg[-HEADER:]}.")
+    elif 'RECVMSG' in recvmsg[HEADER:]:
+        print(f"You have {recvmsg[-HEADER:]} new message(s)!")
+        for i in range(int(recvmsg[-HEADER:])):
             recvhead = int(sock.recv(HEADSIZE).decode("UTF-8"))
             recvfilenm = sock.recv(FILENMSIZE).decode("UTF-8").strip([' ', '\n'])
             os.write('./rx/enc/'+recvfilenm+'.tar.pgp', sock.recv(recvhead))
             print(f"MSG: {recvfilenm}")
         print("Done!")
 
+def compose_msg(body_file, attachment, to_addr, subject, passphrase):
+    shatar = hashlib.sha256(subject).hexdigest()
+    tarfile.open('./tx/dec/'+shatar+'.tar', x)
+    tf = tarfile.open('./tx/dec/'+shatar+'.tar', w)
+    for i in range(len(attachment)):
+        tf.addfile(attachment[i])
+        
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, 293))
+    key,_ = pgpy.PGPKey.from_file('./keys/mine/private.key')
+    key.unlock(passphrase)
+    msg = "SEND "+USRNM, key.sign(to_addr)
+    msghead = len(msg)
+    sock.send(f"{msghead:<{HEADSIZE}}{msg}")
